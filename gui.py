c363ec05924db232dbfc81068105f5702a141c9f
Igor Almeida <inazumaseleven04@gmail.com>
2023-11-12 22:10:21 -0300

    tentando melhorar layout

--- gui.py
+++ gui.py
@@ -18,7 +18,7 @@ def receive_messages(log = print):
 
             formated = str(received_message, encoding="ascii")
 
-            log(f"Received message: {formated}")
+            log(f"Received message:\n{formated}")
 
     except TypeError:
         pass
@@ -31,18 +31,21 @@ def receive_messages(log = print):
 
 class MyWindow(Gtk.Window):
     def __init__(self):
-        super().__init__(title="Pi UART Client", default_width=700, default_height=900)
+        super().__init__(title="Pi UART Client", default_width=600, default_height=900)
         self.set_border_width(10)
 
         t = Thread(target=receive_messages, args=[self.log])
 
+        self.entry = Gtk.Entry()
+        self.entry.set_text("Message from client.")
+
         self.send_button = Gtk.Button(label="Send")
         self.send_button.connect("clicked", self.on_send)
 
-        self.echo_back_button = Gtk.Button(label="Echo Back")
+        self.echo_back_button = Gtk.Button(label="Read Message History")
         self.echo_back_button.connect("clicked", self.on_echo_back)
 
-        self.read_button = Gtk.Button(label="Read")
+        self.read_button = Gtk.Button(label="Read Kernel Messages")
         self.read_button.connect("clicked", self.on_read)
 
         self.clear_history_button = Gtk.Button(label="Clear History")
@@ -56,13 +59,14 @@ class MyWindow(Gtk.Window):
         scroll_window = Gtk.ScrolledWindow(child=self.received_messagens_view)
 
         grid = Gtk.Grid(column_spacing=10, row_spacing=10)
-        grid.attach(self.send_button, 0, 0, 1, 1)
-        grid.attach_next_to(self.echo_back_button, self.send_button, Gtk.PositionType.BOTTOM, 1, 1)
-        grid.attach_next_to(self.read_button, self.echo_back_button, Gtk.PositionType.BOTTOM, 1, 1)
-        grid.attach_next_to(self.clear_history_button, self.read_button, Gtk.PositionType.BOTTOM, 1, 1)
-        grid.attach_next_to(self.clear_log_button, self.clear_history_button, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach(self.entry, 0, 0, 3, 1)
+        grid.attach_next_to(self.send_button, self.entry, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(self.echo_back_button, self.entry, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach_next_to(self.read_button, self.echo_back_button, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(self.clear_history_button, self.read_button, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(self.clear_log_button, self.clear_history_button, Gtk.PositionType.RIGHT, 1, 1)
 
-        box = Gtk.Box(spacing=10)
+        box = Gtk.Box(spacing=10, orientation=Gtk.Orientation.VERTICAL)
         box.pack_start(grid, False, False, 0)
         box.pack_start(scroll_window, True, True, 0)
 
@@ -84,7 +88,7 @@ class MyWindow(Gtk.Window):
         self.received_messagens_view.set_buffer(new_buffer)
 
     def on_send(self, widget):
-        user_message = "Igor de Almeida"
+        user_message = self.entry.get_text()
 
         ascii_bytes = (user_message + "\n").encode("ascii")
 
@@ -93,18 +97,12 @@ class MyWindow(Gtk.Window):
         self.log(f"Message '{user_message}' was sent!")
 
     def on_echo_back(self, widget):
-        self.log("Reading messages that were sent from this client...")
-
         ser.write(b"^")
 
     def on_read(self, widget):
-        self.log("Reading kernel messages...")
-
         ser.write(b"`")
 
     def on_clear_history(self, widget):
-        self.log("Erasing previous messages sent from this client...")
-
         ser.write(b"~")
 
     def on_clear_log(self, widget):
